05ccfad456c99ff13bac072c27121f46d5a28ded
wangkaiwd <1162486176@qq.com>
2020-09-12 17:03:29 +0800

    feat: create simple page structure

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
     <div class="nav">
-      <ul>
-        <li v-for="menu in menus"
+      <ul class="menu">
+        <li class="menu-item" v-for="menu in menus"
             :key="menu.path">
           <router-link :to="menu.path">
             {{ menu.name }}
           </router-link>
         </li>
       </ul>
-
     </div>
-    <router-view></router-view>
+    <div class="content">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 <script>
@@ -26,5 +27,21 @@ export default {
   }
 };
 </script>
-<style lang="scss">
+<style scoped lang="scss">
+#app {
+  display: flex;
+  height: 100vh;
+  overflow: auto;
+  .nav {
+    width: 10em;
+    border-right: 1px solid rgb(66 133 242 / 1);
+    li {
+      padding: 0.2em 0.5em;
+    }
+  }
+  .content {
+    flex: 1;
+    padding: 16px;
+  }
+}
 </style>
