3f5e7011a148fbcb912d4c7cd5e8dfe6252eec45
wangkaiwd <1162486176@qq.com>
2020-09-12 23:59:14 +0800

    feat: extend vue proto method

---
+++ src/shared/proto-extend.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+
+Vue.prototype.$dispatch = function () {
+
+};
---
+++ src/views/extend-proto/demo-extend-proto.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="demo-extend-proto">
+    proto
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DemoExtendProto',
+  data () {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.demo-extend-proto {
+
+}
+</style>
