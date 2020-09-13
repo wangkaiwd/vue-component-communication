e9fe1d2dad97e003f684a98952c79d953b80ce21
wangkaiwd <1162486176@qq.com>
2020-09-13 17:14:58 +0800

    feat: create mind map for vue component communication

---
+++ src/views/bus/demo-bus.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="demo-bus">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DemoBus',
+  data () {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.demo-bus {
+
+}
+</style>
---
+++ src/views/provide-inject/demo-provide-inject.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="demo-provide-inject">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DemoProvideInject',
+  data () {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.demo-provide-inject {
+
+}
+</style>
